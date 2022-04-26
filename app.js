3e09f70f78f6aa957fd259f8d8d2f8219942aa24
mofulbright <mofulbright02@gmail.com>
2022-04-25 22:01:45 -0300

    initial

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Morgan";
+let lastName = "Fulbright";
+var age = 22;
 // Exercise 2
-
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 // Exercise 3
+let city = "Buenos Aires";
+let favorite = "video games";
+let favorite2 = "reading"
+let myStory = `Hello, my name is ${fullName}. I am currently living in ${city}. My favorite pastimes are playing ${favorite} and ${favorite2}`;
+console.log(myStory);
